64ffeae33203e32df7b2e704c92e0e11b4323267
Gabriel ARTHUS <120360026+ParzivalPy@users.noreply.github.com>
2025-10-16 21:41:35 +0200

    fin footer et navbar

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gab'recettes</title>
+    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+</head>
+<body>
+    <?php
+    include_once("includes/navbar.php");
+    ?>
+    <?php
+    include_once("includes/footer.php");
+    ?>
+</body>
+</html>
