e632c7bdcf086d69dba10ab312e05c657fd3f90b
Gabriel ARTHUS <120360026+ParzivalPy@users.noreply.github.com>
2025-10-19 14:47:00 +0200

    fin de la création de la barre de recherche et des filtres

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,9 @@
 <navbar>
     <ul>
         <li class="navbar-point"></li>
-        <a href="index.php"><li class="navbar-menu-button"><div class="navbar-menu-button-img"><span class="material-symbols-outlined">chef_hat</span></div>Découvrir</li></a>
-        <a href="mes_recettes.php"><li class="navbar-menu-button"><div class="navbar-menu-button-img"><span class="material-symbols-outlined">lunch_dining</span></div>Mes recettes</li></a>
-        <a href="compte.php"><li class="navbar-menu-button"><li class="navbar-menu-button"><div class="navbar-menu-button-img"><span class="material-symbols-outlined">account_box</span></div>Mon compte</li></a>
+        <a href="index.php"><li class="navbar-menu-button"><div class="google-icon-container navbar-icon"><span class="material-symbols-outlined">chef_hat</span></div>Découvrir</li></a>
+        <a href="mes_recettes.php"><li class="navbar-menu-button"><div class="google-icon-container navbar-icon"><span class="material-symbols-outlined">lunch_dining</span></div>Mes recettes</li></a>
+        <a href="compte.php"><li class="navbar-menu-button"><li class="navbar-menu-button"><div class="google-icon-container navbar-icon"><span class="material-symbols-outlined">account_box</span></div>Mon compte</li></a>
         <li class="navbar-point"> </li>
     </ul>
 </navbar>
--- index.php
+++ index.php
@@ -11,16 +11,38 @@
     <?php
     include_once("includes/navbar.php");
     ?>
+    <div class="headers">
+        <h1>Trouvez le plat <span>parfait</span></h1>
 
-    <h1>Trouvez le plat <span>parfait</span></h1>
-
-    <div class="search">
-        <div class="bar">
-            <span class="material-symbols-outlined">search</span>
-            <input type="text" placeholder="Chercher une recette">
+        <div class="search">
+            <div class="bar">
+                <span class="material-symbols-outlined">search</span>
+                <input type="text" placeholder="Chercher une recette">
+            </div>
+            <div class="examples">
+                Essayez: <span>‘blanquette de veau’</span> ou <span>‘tarte aux pommes’</span>
+            </div>
         </div>
-        <div class="examples">
-            Essayez: <span>‘blanquette de veau’</span> ou <span>‘tarte aux pommes’</span>
+
+        <div class="filters">
+            <div class="type box">
+                <span class="filter-text">Type</span>
+                <div class="google-icon-container filters-icon">
+                    <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                </div>
+            </div>
+            <div class="time box">
+                <span class="filter-text">Temps de préparation</span>
+                <div class="google-icon-container filters-icon">
+                    <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                </div>
+            </div>
+            <div class="origin box">
+                <span class="filter-text">Origine</span>
+                <div class="google-icon-container filters-icon">
+                    <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                </div>
+            </div>
         </div>
     </div>
 
